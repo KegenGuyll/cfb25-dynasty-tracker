973c0f977b255e84609dafb5e469a6e368bd3d90
Kegen Guyll <guyllkegen@gmail.com>
2024-08-27 18:57:30 -0500

    chore: Update page component to display dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import { Button } from "@nextui-org/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="space-y-12">
-        <div>
-          <h1>Load existing save</h1> 
-          <input type='file' accept="application/JSON" />
-        </div>
-        <div>
-          <h1>Start new game</h1>
-          <Button>Start</Button>
-        </div>
-      </div>
-    </main>
+    <div>
+      <h1>Dashboard</h1>
+    </div>
   );
 }

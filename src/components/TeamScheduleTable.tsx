745dbd10b1112f0c192b74e738225504e9402c8f
Kegen Guyll <guyllkegen@gmail.com>
2024-10-01 19:13:45 -0500

    removed nextui table

--- src/components/TeamScheduleTable.tsx
+++ src/components/TeamScheduleTable.tsx
@@ -1,102 +1,144 @@
-import React from "react";
-import {  
-  Table,  
-  TableHeader,  
-  TableBody,  
-  TableColumn,  
-  TableRow,  
-  TableCell
-} from "@nextui-org/table";
-import TeamSelect from "./TeamSelect";
-import { Select, SelectItem } from "@nextui-org/select";
-import { Input } from "@nextui-org/input";
-import { Control, Controller } from "react-hook-form";
-import { CreateTeamScheduleForm } from "@/app/team-schedule/create/page";
+import React from 'react'
+import TeamSelect from './TeamSelect'
+import { Select, SelectItem } from '@nextui-org/select'
+import { Input } from '@nextui-org/input'
+import { Control, Controller } from 'react-hook-form'
+import { CreateTeamScheduleForm } from '@/app/team-schedule/create/page'
 
 type TeamScheduleTableProps = {
   numberOfWeeks: number
-  teamOptions: { value: string, label: string }[]
+  teamOptions: { value: string; label: string }[]
   control: Control<CreateTeamScheduleForm, any>
 }
 
-const TeamScheduleTable: React.FC<TeamScheduleTableProps> = (
-  {
-    teamOptions,
-    numberOfWeeks,
-    control
-  }: TeamScheduleTableProps) => {
+const TeamScheduleTable: React.FC<TeamScheduleTableProps> = ({
+  teamOptions,
+  numberOfWeeks,
+  control,
+}: TeamScheduleTableProps) => {
   return (
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>WEEK</TableColumn>
-        <TableColumn>LOCATION</TableColumn>
-        <TableColumn>OPPONENT</TableColumn>
-        <TableColumn>STADIUM</TableColumn>
-      </TableHeader>
-      <TableBody>
+    <table className="table-auto border-separate rounded bg-default-50">
+      <thead className="bg-default-100 text-center uppercase text-md font-semibold rounded-t">
+        <tr>
+          <th scope="col" className="px-4 py-3 text-center bg-default-100">
+            Week
+          </th>
+          <th className="px-4 py-3">Location</th>
+          <th className="px-4 py-3">Opponent</th>
+          <th className="px-4 py-3">Final Score</th>
+          <th className="px-4 py-3">Result</th>
+        </tr>
+      </thead>
+      <tbody className="gap-4">
         {Array.from({ length: numberOfWeeks }, (_, i) => i).map((week, i) => (
-          <TableRow key={week.toString()}>
-            <TableCell className="w-[45px]" >
-              <span className="w-full text-center" >{week}</span>
-            </TableCell>   
-            <TableCell tabIndex={week}>
+          <tr tabIndex={-1} key={week.toString()}>
+            <td className="px-4 py-3 text-center">
+              <span className="w-full text-center">{week}</span>
+            </td>
+            <td className="px-4 py-3 flex items-center justify-center">
               <Controller
                 control={control}
                 name={`games.${i}.location`}
                 render={({ field }) => (
                   <Select
+                    tabIndex={0}
                     className="max-w-[80px]"
                     fullWidth
                     size="lg"
                     {...field}
                   >
                     {['VS', 'AT', 'BYE'].map((location) => (
-                      <SelectItem 
-                        key={location} 
-                        value={location}
-                      >
-                          {location}
+                      <SelectItem key={location} value={location}>
+                        {location}
                       </SelectItem>
                     ))}
                   </Select>
                 )}
               />
-            </TableCell>
-            <TableCell className="max-w-[300px]">
+            </td>
+            <td className="px-4 py-3">
               <Controller
                 control={control}
                 name={`games.${i}.opponent`}
-                render={({ field: {value, onChange} }) => (
-                  <TeamSelect 
+                render={({ field: { value, onChange } }) => (
+                  <TeamSelect
                     label="Opponent"
-                    value={teamOptions?.find((option) => option.value === value)}
+                    value={teamOptions?.find(
+                      (option) => option.value === value
+                    )}
                     onChange={onChange}
-                    placeholder="Select Opponent" 
-                    options={teamOptions} 
+                    placeholder="Select Opponent"
+                    options={teamOptions}
                   />
                 )}
               />
-            </TableCell>
-            <TableCell className="max-w-[200px]">
+            </td>
+            <td className="px-4 py-3 flex items-center justify-center">
+              <div className="flex items-center flex-row gap-x-1">
+                <div className="max-w-[65px]">
+                  <Controller
+                    control={control}
+                    name={`games.${i}.finalScore.score1`}
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        tabIndex={0}
+                        placeholder="0"
+                        fullWidth
+                        size="lg"
+                        value={value || ''}
+                        onChange={onChange}
+                        type="number"
+                      />
+                    )}
+                  />
+                </div>
+                <span className="font-bold text-xl">-</span>
+                <div className="max-w-[65px]">
+                  <Controller
+                    control={control}
+                    name={`games.${i}.finalScore.score2`}
+                    render={({ field: { onChange, value } }) => (
+                      <Input
+                        tabIndex={0}
+                        placeholder="0"
+                        fullWidth
+                        size="lg"
+                        value={value || ''}
+                        onChange={onChange}
+                        type="number"
+                      />
+                    )}
+                  />
+                </div>
+              </div>
+            </td>
+            <td className="px-4 py-3">
               <Controller
                 control={control}
-                name={`games.${i}.stadium`}
-                render={({ field: { onChange, value} }) => (
-                  <Input 
-                    label="Stadium name"
+                name={`games.${i}.location`}
+                render={({ field }) => (
+                  <Select
+                    tabIndex={0}
+                    id="Test Id2"
+                    className="max-w-[65px]"
                     fullWidth
-                    size='lg' 
-                    value={value || ''}
-                    onChange={onChange}
-                  />
+                    size="lg"
+                    {...field}
+                  >
+                    {['W', 'L'].map((result) => (
+                      <SelectItem key={result} value={result}>
+                        {result}
+                      </SelectItem>
+                    ))}
+                  </Select>
                 )}
               />
-            </TableCell>
-          </TableRow>
+            </td>
+          </tr>
         ))}
-      </TableBody>
-    </Table>
-  );
+      </tbody>
+    </table>
+  )
 }
 
-export default TeamScheduleTable;
+export default TeamScheduleTable

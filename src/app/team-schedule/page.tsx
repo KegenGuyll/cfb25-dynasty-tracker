57a84e704456e79b8a186130db4348d06488a7ba
Kegen Guyll <guyllkegen@gmail.com>
2024-10-02 15:13:27 -0500

    created table to display team schedules

--- src/app/team-schedule/page.tsx
+++ src/app/team-schedule/page.tsx
@@ -1,44 +1,106 @@
 'use client'
 
-import getTeamScheduleWithTeam from "@/db/functions/getTeamScheduleWithTeam";
-import { Button } from "@nextui-org/button";
-import { useLiveQuery } from "dexie-react-hooks";
-import Link from "next/link";
+import getTeamScheduleWithTeam from '@/db/functions/getTeamScheduleWithTeam'
+import { Game } from '@/db/types'
+import { Button } from '@nextui-org/button'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from '@nextui-org/table'
+import { useLiveQuery } from 'dexie-react-hooks'
+import Link from 'next/link'
+
+const determineGameResult = (game: Game): string => {
+  const didTeamWin =
+    game.finalScore?.home &&
+    game.finalScore?.away &&
+    game.finalScore.home > game.finalScore.away
+
+  if (game.finalScore?.home || game.finalScore?.away) {
+    return ` ${didTeamWin ? 'W' : 'L'} ${game.finalScore?.home} - ${
+      game.finalScore?.away
+    }`
+  }
+
+  if (game.location === 'bye') return 'BYE'
+
+  return 'TBD'
+}
+
+const determineTeamName = (game: Game): string => {
+  if (game.location === 'home') {
+    return `${game.homeTeam?.school}` || ''
+  }
+
+  if (game.location === 'bye') {
+    return 'BYE WEEK'
+  }
+
+  return `${game.awayTeam?.school}` || ''
+}
 
 const TeamSchedulePage = () => {
-  const allTeamSchedules = useLiveQuery(() => getTeamScheduleWithTeam());
+  const allTeamSchedules = useLiveQuery(() => getTeamScheduleWithTeam())
 
-  if(!allTeamSchedules?.length) {
+  if (!allTeamSchedules?.length) {
     return (
       <div>
         <h1>No Team Schedules</h1>
-        <Button as={Link} href='/team-schedule/create'>Create</Button>
+        <Button as={Link} href="/team-schedule/create">
+          Create
+        </Button>
       </div>
     )
   }
 
   return (
     <div className="flex flex-col gap-8">
-      <h1 className="text-xl font-semibold" >Team Schedules</h1>
+      <h1 className="text-xl font-semibold">Team Schedules</h1>
       {allTeamSchedules?.map((teamSchedule) => (
         <div key={teamSchedule.id}>
-          {`${teamSchedule.team?.school} ${teamSchedule.team?.nickname}`} - {teamSchedule.year}
-          {teamSchedule.games.length && (
-            <div className="pl-8">
-              <ul>
-                {teamSchedule.games.map((game) => (
-                  <li key={game.week}>
-                    Week {game.week} / {game.location === 'home' ? 'VS' : 'AT'} / {game.location === 'away' ? game.awayTeam?.nickname : game.homeTeam?.nickname}
-                  </li>
-                ))}
-              </ul>
+          <div className="flex flex-row items-center gap-4 mb-4">
+            <div>
+              <h1 className="font-semibold">Team Name</h1>
+              <span className="font-light text-sm">
+                {teamSchedule.team?.school} {teamSchedule.team?.nickname}
+              </span>
             </div>
-          )}
+            <div className="flex-grow">
+              <h1 className="font-semibold">Year</h1>
+              <span className="font-light text-sm">{teamSchedule.year}</span>
+            </div>
+            <div className="flex gap-2">
+              <Button>Update</Button>
+              <Button color="danger">Remove</Button>
+            </div>
+          </div>
+          <Table>
+            <TableHeader>
+              <TableColumn>WEEK</TableColumn>
+              <TableColumn>OPPONENT</TableColumn>
+              <TableColumn>RESULT</TableColumn>
+            </TableHeader>
+            <TableBody>
+              {teamSchedule.games.map((game) => (
+                <TableRow key={game.week}>
+                  <TableCell>{game.week - 1}</TableCell>
+                  <TableCell>{determineTeamName(game)}</TableCell>
+                  <TableCell>{determineGameResult(game)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       ))}
-      <Button as={Link} href='/team-schedule/create' >New Create Schedule</Button>
+      <Button as={Link} href="/team-schedule/create">
+        New Create Schedule
+      </Button>
     </div>
-  );
+  )
 }
 
-export default TeamSchedulePage;
+export default TeamSchedulePage

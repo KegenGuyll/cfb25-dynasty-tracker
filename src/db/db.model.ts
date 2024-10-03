a5adb5213cc9f742e8aa5141d1908f84f7a69836
Kegen Guyll <guyllkegen@gmail.com>
2024-10-03 15:10:47 -0500

    created initialAward data

--- src/db/db.model.ts
+++ src/db/db.model.ts
@@ -1,6 +1,7 @@
 import Dexie, { EntityTable, Transaction } from 'dexie';
-import { Awards, DraftResults, RecruitingClass, Team, TeamSchedule, TeamStats } from './types';
+import { AvailableAwards, Awards, DraftResults, RecruitingClass, Team, TeamSchedule, TeamStats } from './types';
 import initialTeams from '../data/initialTeamData.json';
+import initialAwards from '../data/initialAwardData.json';
 
 
 const db = new Dexie('cfbDynastyTracker') as Dexie & {
@@ -9,6 +10,7 @@ const db = new Dexie('cfbDynastyTracker') as Dexie & {
   teamSchedule: EntityTable<TeamSchedule, 'id'>;
   recruitingClass: EntityTable<RecruitingClass, 'id'>;
   awards: EntityTable<Awards, 'id'>;
+  availableAwards: EntityTable<AvailableAwards, 'id'>;
   teams: EntityTable<Team, 'id'>;
 }
 
@@ -18,12 +20,14 @@ db.version(1).stores({
   teamSchedule: '++id, teamId, year',
   recruitingClass: '++id,teamId, year',
   awards: '++id,trophy,playerName,teamId, year',
-  teams: '++id,teamId'
+  teams: '++id,teamId',
+  availableAwards: '++id,name,awardId'
 });
 
 db.on('populate', async (tx: Transaction) => {
   console.log('Populating database with initial data')
   await tx.table('teams').bulkAdd(initialTeams)
+  await tx.table('availableAwards').bulkAdd(initialAwards)
 })
 
 export { db };

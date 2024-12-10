6ff6b457cec2a223cf37d5d4f60bec607a20fdf2
Kegen Guyll <guyllkegen@gmail.com>
2024-12-10 11:39:46 -0600

    created awards page

--- src/app/awards/page.tsx
+++ src/app/awards/page.tsx
@@ -1,11 +1,24 @@
 'use client'
 
 import { awardsCreateUrl } from '@/constants/urls'
+import { db } from '@/db/db.model'
+import getAwardsWithTeam from '@/db/functions/getAwardsWithTeam'
 import { Button } from '@nextui-org/button'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from '@nextui-org/table'
+import { useLiveQuery } from 'dexie-react-hooks'
 import { useRouter } from 'next/navigation'
 
 const AwardsPage = () => {
   const router = useRouter()
+  const savedAwards = useLiveQuery(() => getAwardsWithTeam())
+  const availableAwards = useLiveQuery(() => db.availableAwards.toArray())
 
   return (
     <div>
@@ -13,6 +26,33 @@ const AwardsPage = () => {
       <Button onClick={() => router.push(awardsCreateUrl)}>
         Add Award Winner
       </Button>
+      <div className="flex flex-col gap-4 mt-4">
+        {availableAwards?.map((award) => (
+          <div key={award.awardId}>
+            <h2>{award.name}</h2>
+            <Table>
+              <TableHeader>
+                <TableColumn>Team</TableColumn>
+                <TableColumn>Name</TableColumn>
+                <TableColumn>Year</TableColumn>
+              </TableHeader>
+              <TableBody>
+                {savedAwards
+                  ?.filter((a) => a.awardId === award.awardId)
+                  .map((award) => (
+                    <TableRow key={award.id}>
+                      <TableCell>{award.team?.school}</TableCell>
+                      <TableCell>
+                        {award.playerFirstName} {award.playerLastName}
+                      </TableCell>
+                      <TableCell>{award.year}</TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }

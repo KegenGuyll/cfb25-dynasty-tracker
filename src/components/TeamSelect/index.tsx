a5052d37f0aef96c093ba7338cd3f8184d44c434
Kegen Guyll <guyllkegen@gmail.com>
2024-12-11 14:40:51 -0600

    created a real TeamSelect component

---
+++ src/components/TeamSelect/index.tsx
@@ -0,0 +1,37 @@
+import getTeamSelectOptions from '@/db/functions/getTeamSelectOptions'
+import { useLiveQuery } from 'dexie-react-hooks'
+import SearchableSelect from '../SearchableSelect'
+import classNames from 'classnames'
+
+type Props = {
+  value: string
+  onChange?: (value: string | undefined) => void
+  errors?: Record<string, { message: string }>
+  classname?: string
+}
+
+const TeamSelect: React.FC<Props> = ({
+  value,
+  onChange,
+  errors,
+  classname,
+}: Props) => {
+  const teamOptions = useLiveQuery(() => getTeamSelectOptions())
+
+  return (
+    <div className={classNames(classname, 'w-full')}>
+      <SearchableSelect
+        options={teamOptions || []}
+        label="Team"
+        placeholder="Select Team"
+        value={teamOptions?.find((option) => option.value === value)}
+        onChange={onChange}
+        isInvalid={errors?.teamId?.message ? true : false}
+        isRequired
+      />
+      <span className="text-xs text-danger">{errors?.teamId?.message}</span>
+    </div>
+  )
+}
+
+export default TeamSelect

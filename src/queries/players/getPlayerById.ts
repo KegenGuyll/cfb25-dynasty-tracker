73d26079b8220f535d3035db3d4ce3c1217fafed
Kegen Guyll <guyllkegen@gmail.com>
2025-01-14 16:25:23 -0600

    Add team data retrieval for player stats in getPlayerById query

--- src/queries/players/getPlayerById.ts
+++ src/queries/players/getPlayerById.ts
@@ -7,6 +7,30 @@ const getPlayerById = async (dynastyId: string, playerId: string) => {
 
   result.currentTeamData = await db.teams.where({ id: result.teamId }).first()
 
+  if (result.stats.passing) {
+    Promise.all(result.stats.passing.map(async (stat) => {
+      stat.teamData = await db.teams.where({ id: stat.teamId }).first()
+    }))
+  }
+
+  if (result.stats.rushing) {
+    Promise.all(result.stats.rushing.map(async (stat) => {
+      stat.teamData = await db.teams.where({ id: stat.teamId }).first()
+    }))
+  }
+
+  if (result.stats.receiving) {
+    Promise.all(result.stats.receiving.map(async (stat) => {
+      stat.teamData = await db.teams.where({ id: stat.teamId }).first()
+    }))
+  }
+
+  if (result.stats.defense) {
+    Promise.all(result.stats.defense.map(async (stat) => {
+      stat.teamData = await db.teams.where({ id: stat.teamId }).first()
+    }))
+  }
+
   return result;
 }
 

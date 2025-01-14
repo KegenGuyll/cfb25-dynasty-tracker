dd11ebdec7f15314250ae73ee8fba67088229436
Kegen Guyll <guyllkegen@gmail.com>
2025-01-14 12:38:38 -0600

    Add EditPlayerNumber component for player information editing

---
+++ src/components/Player/EditPlayerNumber.tsx
@@ -0,0 +1,111 @@
+import {
+  Player,
+  playerDevTraitOptions,
+  playerPositionOptions,
+} from '@/db/types/player'
+import EditModal from '../Modal/EditModal'
+import { Controller, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Input, Select, SelectItem } from '@nextui-org/react'
+import { db } from '@/db/db.model'
+
+type EditPlayerGeneralInformationProps = {
+  player: Player
+  isOpen: boolean
+  handleClose: () => void
+}
+
+export const generalInformationSchema = yup.object({
+  number: yup.string().nullable(),
+  position: yup.string().optional(),
+  devTrait: yup.string().optional(),
+})
+
+type GeneralInformationFormData = yup.InferType<typeof generalInformationSchema>
+
+const EditGeneralInformation: React.FC<EditPlayerGeneralInformationProps> = ({
+  player,
+  isOpen,
+  handleClose,
+}: EditPlayerGeneralInformationProps) => {
+  const { control, handleSubmit } = useForm<GeneralInformationFormData>({
+    resolver: yupResolver(generalInformationSchema),
+    defaultValues: {
+      number: String(player.information.number) || null,
+      position: player.information.position,
+      devTrait: player.development.devTrait,
+    },
+  })
+
+  const onSubmit = async (data: GeneralInformationFormData) => {
+    await db.players.update(player.id, {
+      'information.number': Number(data.number),
+      'information.position': data.position,
+      'development.devTrait': data.devTrait,
+    })
+    handleClose()
+  }
+
+  return (
+    <EditModal
+      handleClose={handleClose}
+      isOpen={isOpen}
+      formId="general-information-form"
+      title={`${player.information.firstName} ${player.information.lastName}`}
+    >
+      <form
+        className="flex flex-col gap-4"
+        onSubmit={handleSubmit(onSubmit)}
+        id="general-information-form"
+      >
+        <Controller
+          name="number"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <Input
+              value={String(value)}
+              onChange={onChange}
+              label="Number"
+              placeholder="Number"
+            />
+          )}
+        />
+        <Controller
+          name="position"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <Select
+              label="Pos."
+              value={value}
+              defaultSelectedKeys={[value || '']}
+              onChange={onChange}
+            >
+              {playerPositionOptions.map((position) => (
+                <SelectItem key={position.key}>{position.label}</SelectItem>
+              ))}
+            </Select>
+          )}
+        />
+        <Controller
+          name="devTrait"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <Select
+              label="Dev Trait"
+              value={value}
+              defaultSelectedKeys={[value || '']}
+              onChange={onChange}
+            >
+              {playerDevTraitOptions.map((trait) => (
+                <SelectItem key={trait.key}>{trait.label}</SelectItem>
+              ))}
+            </Select>
+          )}
+        />
+      </form>
+    </EditModal>
+  )
+}
+
+export default EditGeneralInformation
--- src/components/Player/PlayerCard.tsx
+++ src/components/Player/PlayerCard.tsx
@@ -3,6 +3,7 @@ import { faPen } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Divider } from '@nextui-org/react'
 import { useState } from 'react'
+import EditGeneralInformation from './EditPlayerNumber'
 
 type PlayerCardProps = {
   player: Player | null | undefined
@@ -46,76 +47,87 @@ const PlayerSection: React.FC<PlayerSectionProps> = ({
 }
 
 const PlayerCard = ({ player }: PlayerCardProps) => {
+  const [editGeneralInformation, setEditGeneralInformation] = useState(false)
   const playerName = `${player?.information.firstName} ${player?.information.lastName}`
 
   if (!player) return null
 
   return (
-    <div className="flex flex-col gap-4 w-full border rounded p-6">
-      <caption className="text-center text-lg w-full font-semibold">
-        {playerName}
-      </caption>
-      <PlayerSection title={`No. ${player.information.number}`}>
-        <ul>
-          <li>Position: {player.information.position}</li>
-          <li>Dev Trait: {player.development.devTrait}</li>
-        </ul>
-      </PlayerSection>
-      <PlayerSection title="Abilities">
-        <div>
-          <h3>Tendency: {player.information.tendency}</h3>
-          <h3>Mental -</h3>
-          <ul className="pl-4">
-            {player.development.mentalTraits?.map((trait) => (
-              <li key={trait.trait}>
-                {trait.trait} ({trait.tier})
+    <>
+      <div className="flex flex-col gap-4 w-full border rounded p-6">
+        <caption className="text-center text-lg w-full font-semibold">
+          {playerName}
+        </caption>
+        <PlayerSection
+          onEdit={() => setEditGeneralInformation(true)}
+          title={`No. ${player.information.number}`}
+        >
+          <ul>
+            <li>Position: {player.information.position}</li>
+            <li>Dev Trait: {player.development.devTrait}</li>
+          </ul>
+        </PlayerSection>
+        <PlayerSection title="Abilities">
+          <div>
+            <h3>Tendency: {player.information.tendency}</h3>
+            <h3>Mental -</h3>
+            <ul className="pl-4">
+              {player.development.mentalTraits?.map((trait) => (
+                <li key={trait.trait}>
+                  {trait.trait} ({trait.tier})
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h3>Physical -</h3>
+            <ul className="pl-4">
+              {player.development.physicalTraits?.map((trait) => (
+                <li key={trait.trait}>
+                  {trait.trait} ({trait.tier})
+                </li>
+              ))}
+            </ul>
+          </div>
+        </PlayerSection>
+        <Divider />
+        <PlayerSection title="Personal Information">
+          <ul>
+            <li>Hometown: {player.information.hometown}</li>
+            <li>Height: {player.information.height}</li>
+            <li>Weight: {player.information.weight}</li>
+          </ul>
+        </PlayerSection>
+        <Divider />
+        <PlayerSection title="Career Information">
+          <ul>
+            <li>College: {player.currentTeamData?.school} (years-played)</li>
+            <li>NFL Draft: year / round : number</li>
+          </ul>
+        </PlayerSection>
+        <Divider />
+        <PlayerSection title="Career Highlights and Awards">
+          <ul>
+            {player.awards.map((award) => (
+              <li key={award.awardId}>
+                {award.awardName} ({award.year})
               </li>
             ))}
           </ul>
-        </div>
-        <div>
-          <h3>Physical -</h3>
-          <ul className="pl-4">
-            {player.development.physicalTraits?.map((trait) => (
-              <li key={trait.trait}>
-                {trait.trait} ({trait.tier})
-              </li>
-            ))}
+        </PlayerSection>
+        <Divider />
+        <PlayerSection title="Career Statistics">
+          <ul>
+            <li>Will be determined by position</li>
           </ul>
-        </div>
-      </PlayerSection>
-      <Divider />
-      <PlayerSection title="Personal Information">
-        <ul>
-          <li>Hometown: {player.information.hometown}</li>
-          <li>Height: {player.information.height}</li>
-          <li>Weight: {player.information.weight}</li>
-        </ul>
-      </PlayerSection>
-      <Divider />
-      <PlayerSection title="Career Information">
-        <ul>
-          <li>College: {player.currentTeamData?.school} (years-played)</li>
-          <li>NFL Draft: year / round : number</li>
-        </ul>
-      </PlayerSection>
-      <Divider />
-      <PlayerSection title="Career Highlights and Awards">
-        <ul>
-          {player.awards.map((award) => (
-            <li key={award.awardId}>
-              {award.awardName} ({award.year})
-            </li>
-          ))}
-        </ul>
-      </PlayerSection>
-      <Divider />
-      <PlayerSection title="Career Statistics">
-        <ul>
-          <li>Will be determined by position</li>
-        </ul>
-      </PlayerSection>
-    </div>
+        </PlayerSection>
+      </div>
+      <EditGeneralInformation
+        isOpen={editGeneralInformation}
+        handleClose={() => setEditGeneralInformation(false)}
+        player={player}
+      />
+    </>
   )
 }
 

394107256e3765354702532a7702ba24c0f97faf
Kegen Guyll <guyllkegen@gmail.com>
2025-01-15 09:50:23 -0600

    Enhance getAllTeamSeason to include team statistics and schedules

--- src/queries/dynasty/getAllTeamSeason.ts
+++ src/queries/dynasty/getAllTeamSeason.ts
@@ -1,5 +1,16 @@
+import { AvailableTeams } from "@/db/types/dynasty"
 import getDynastyById from "./getDynastyById"
+import { TeamSchedule, TeamStats } from "@/db/types"
+import { RecruitingClass } from "@/db/types/recruiting"
+import { TeamInfo } from "@/db/types/teamInfo"
+import { db } from "@/db/db.model"
 
+type AvailableTeamsWithStats = AvailableTeams & {
+  teamSchedule?: TeamSchedule
+  teamStats?: TeamStats
+  teamInfo?: TeamInfo
+  recruitingClass?: RecruitingClass
+}
 
 const getAllTeamSeason = async (dynastyId: number, teamId: number) => {
   const dynasties = await getDynastyById(dynastyId)
@@ -8,7 +19,28 @@ const getAllTeamSeason = async (dynastyId: number, teamId: number) => {
 
   const teamSeasons = dynasties.availableTeams.filter((team) => team.teamId === teamId)
 
-  return teamSeasons
+  const teamWithStats: AvailableTeamsWithStats[] = teamSeasons.map((team) => {
+    return {
+      ...team,
+      teamSchedule: undefined,
+      teamStats: undefined,
+      teamInfo: undefined,
+      recruitingClass: undefined,
+    }
+  })
+
+  await Promise.all(teamWithStats.map(async (team) => {
+    const searchQuery = { teamId: team.teamId, dynastyId: dynastyId, year: team.year }
+
+    team.teamSchedule = await db.teamSchedule.get(searchQuery)
+    team.teamStats = await db.teamStats.get(searchQuery)
+    team.teamInfo = await db.teamInfo.get(searchQuery)
+    team.recruitingClass = await db.recruitingClass.get(searchQuery)
+  }))
+
+  return teamWithStats
 }
+
+export type { AvailableTeamsWithStats }
 
 export default getAllTeamSeason

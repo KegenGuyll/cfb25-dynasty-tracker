56c9f9bc77ff5b1a7be7fe4d5534bc8026a053dc
Kegen Guyll <guyllkegen@gmail.com>
2025-01-15 12:36:07 -0600

    Add FileUploadButton component for file upload functionality

---
+++ src/components/FileUploadButton.tsx
@@ -0,0 +1,33 @@
+import { Button, ButtonProps } from '@nextui-org/react'
+import { ChangeEvent, useRef } from 'react'
+
+type FileUploadButtonProps = ButtonProps & {
+  fileAccept?: string
+  onUpload: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+const FileUploadButton: React.FC<FileUploadButtonProps> = ({
+  children,
+  fileAccept,
+  onUpload: fileOnChange,
+  ...props
+}: FileUploadButtonProps) => {
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  return (
+    <>
+      <input
+        onChange={fileOnChange}
+        accept={fileAccept}
+        className="hidden"
+        ref={fileInputRef}
+        type="file"
+      />
+      <Button onPress={() => fileInputRef.current?.click()} {...props}>
+        {children}
+      </Button>
+    </>
+  )
+}
+
+export default FileUploadButton

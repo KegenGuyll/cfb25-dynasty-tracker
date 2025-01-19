389fc6f991c5f3537b487c3cbd754b4145bd8de0
Kegen Guyll <guyllkegen@gmail.com>
2025-01-19 11:51:30 -0600

    added ability to create custom game names, update games there were considered a bye

--- src/components/Player/EditGame.tsx
+++ src/components/Player/EditGame.tsx
@@ -1,13 +1,17 @@
-import { Game } from '@/db/types'
+import { Game, GameLocation } from '@/db/types'
 import EditModal from '../Modal/EditModal'
 import { Controller, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Input, Select, SelectItem } from '@nextui-org/react'
 import { db } from '@/db/db.model'
+import TeamSelect from '../TeamSelect'
+import { useLiveQuery } from 'dexie-react-hooks'
+import getTeamSelectOptions from '@/db/functions/getTeamSelectOptions'
+import { useEffect } from 'react'
 
 type EditGameProps = {
-  game: Game
+  game?: Game
   isOpen: boolean
   handleClose: () => void
   scheduleId: number
@@ -17,27 +21,29 @@ export const gameSchema = yup.object({
   awayTeamId: yup.number().required('Away Team is required'),
   homeTeamId: yup.number().required('Home Team is required'),
   result: yup.string().required('Result is required'),
+  gameLocation: yup.string().required(),
+  customGameName: yup.string().optional(),
   scoreSummary: yup
     .object({
       '1': yup.object({
-        home: yup.number().required('Home Score is required'),
-        away: yup.number().required('Away Score is required'),
+        home: yup.number().required('Home Score is required').nullable(),
+        away: yup.number().required('Away Score is required').nullable(),
       }),
       '2': yup.object({
-        home: yup.number().required('Home Score is required'),
-        away: yup.number().required('Away Score is required'),
+        home: yup.number().required('Home Score is required').nullable(),
+        away: yup.number().required('Away Score is required').nullable(),
       }),
       '3': yup.object({
-        home: yup.number().required('Home Score is required'),
-        away: yup.number().required('Away Score is required'),
+        home: yup.number().required('Home Score is required').nullable(),
+        away: yup.number().required('Away Score is required').nullable(),
       }),
       '4': yup.object({
-        home: yup.number().required('Home Score is required'),
-        away: yup.number().required('Away Score is required'),
+        home: yup.number().required('Home Score is required').nullable(),
+        away: yup.number().required('Away Score is required').nullable(),
       }),
       final: yup.object({
-        home: yup.number().required('Home Score is required'),
-        away: yup.number().required('Away Score is required'),
+        home: yup.number().required('Home Score is required').nullable(),
+        away: yup.number().required('Away Score is required').nullable(),
       }),
     })
     .nullable(),
@@ -51,63 +57,87 @@ const EditGame: React.FC<EditGameProps> = ({
   handleClose,
   scheduleId,
 }: EditGameProps) => {
-  const { control, handleSubmit } = useForm({
+  const teamOptions = useLiveQuery(() => getTeamSelectOptions(), [])
+
+  console.log(game)
+
+  const {
+    control,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(gameSchema),
-    defaultValues: {
-      homeTeamId: game.homeTeamId,
-      awayTeamId: game.awayTeamId,
-      result: game.result || '',
-      scoreSummary: {
-        '1': {
-          home: game.scoreSummary ? game.scoreSummary['1'].home : undefined,
-          away: game.scoreSummary ? game.scoreSummary['1'].away : undefined,
-        },
-        '2': {
-          home: game.scoreSummary ? game.scoreSummary['2'].home : undefined,
-          away: game.scoreSummary ? game.scoreSummary['2'].away : undefined,
-        },
-        '3': {
-          home: game.scoreSummary ? game.scoreSummary['3'].home : undefined,
-          away: game.scoreSummary ? game.scoreSummary['3'].away : undefined,
-        },
-        '4': {
-          home: game.scoreSummary ? game.scoreSummary['4'].home : undefined,
-          away: game.scoreSummary ? game.scoreSummary['4'].away : undefined,
-        },
-        final: {
-          home: game.scoreSummary
-            ? game.scoreSummary.final.home
-            : game.finalScore?.home,
-          away: game.scoreSummary
-            ? game.scoreSummary.final.away
-            : game.finalScore?.away,
-        },
-      },
-    },
   })
+
+  useEffect(() => {
+    if (!game) return
+
+    setValue('homeTeamId', game.homeTeamId)
+    setValue('awayTeamId', game.awayTeamId)
+    setValue('result', game.result || '')
+    setValue('gameLocation', game.location)
+    setValue('customGameName', game.customGameName)
+    setValue('scoreSummary', {
+      '1': {
+        home: game.scoreSummary ? game.scoreSummary['1'].home : null,
+        away: game.scoreSummary ? game.scoreSummary['1'].away : null,
+      },
+      '2': {
+        home: game.scoreSummary ? game.scoreSummary['2'].home : null,
+        away: game.scoreSummary ? game.scoreSummary['2'].away : null,
+      },
+      '3': {
+        home: game.scoreSummary ? game.scoreSummary['3'].home : null,
+        away: game.scoreSummary ? game.scoreSummary['3'].away : null,
+      },
+      '4': {
+        home: game.scoreSummary ? game.scoreSummary['4'].home : null,
+        away: game.scoreSummary ? game.scoreSummary['4'].away : null,
+      },
+      final: {
+        home: game.scoreSummary
+          ? game.scoreSummary.final.home
+          : game.finalScore?.home || null,
+        away: game.scoreSummary
+          ? game.scoreSummary.final.away
+          : game.finalScore?.away || null,
+      },
+    })
+  }, [game])
+
+  const watchedHomeTeamId = watch('homeTeamId')
+  const watchedAwayTeamId = watch('awayTeamId')
 
   const handleSave = async (data: GameFormData) => {
     console.log(data)
 
     await db.teamSchedule.update(scheduleId, (schedule) => {
-      const gameIndex = schedule.games.findIndex((g) => g.week === game.week)
+      const gameIndex = schedule.games.findIndex((g) => g.week === game?.week)
       schedule.games[gameIndex] = {
         ...schedule.games[gameIndex],
+        location: data.gameLocation as GameLocation,
+        homeTeamId: data.homeTeamId,
+        awayTeamId: data.awayTeamId,
         result: data.result,
-        finalScore: data.scoreSummary?.final || null,
-        scoreSummary: data.scoreSummary,
+        finalScore: (data.scoreSummary?.final as any) || null,
+        scoreSummary: data.scoreSummary as any,
+        customGameName: data.customGameName || undefined,
       }
     })
 
     handleClose()
   }
 
+  if (!game) return null
+
   return (
     <EditModal
       handleClose={handleClose}
       isOpen={isOpen}
       formId="edit-game-form"
-      title={`Week ${game.week}`}
+      title={`Week ${game?.week}`}
       size="2xl"
     >
       <form
@@ -116,14 +146,14 @@ const EditGame: React.FC<EditGameProps> = ({
         id="edit-game-form"
       >
         <div className="flex flex-col gap-2">
-          <h2 className="text-lg font-semibold">Game Result</h2>
-          <div className="w-20">
+          <div>
             <Controller
               control={control}
               name="result"
               render={({ field: { value, onChange } }) => {
                 return (
                   <Select
+                    label="Game Result"
                     value={value || ''}
                     selectedKeys={[String(value)]}
                     defaultSelectedKeys={[String(value)]}
@@ -140,6 +170,74 @@ const EditGame: React.FC<EditGameProps> = ({
             />
           </div>
         </div>
+        <Controller
+          control={control}
+          name="customGameName"
+          render={({ field }) => (
+            <Input
+              label="Custom Game Name"
+              placeholder="Game Name (Bowl Game, Playoff Game, etc.)"
+              {...field}
+            />
+          )}
+        />
+        {game.awayTeamId === game.homeTeamId && (
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg font-semibold">Set Opponents</h2>
+            <Controller
+              control={control}
+              name="awayTeamId"
+              render={({ field: { value, onChange } }) => {
+                return (
+                  <label>
+                    <span>Away Team</span>
+                    <TeamSelect
+                      teamOptions={teamOptions}
+                      value={String(value)}
+                      onChange={onChange}
+                    />
+                  </label>
+                )
+              }}
+            />
+            <Controller
+              control={control}
+              name="homeTeamId"
+              render={({ field: { value, onChange } }) => {
+                return (
+                  <label>
+                    <span>Home Team</span>
+                    <TeamSelect
+                      teamOptions={teamOptions}
+                      value={String(value)}
+                      onChange={onChange}
+                    />
+                  </label>
+                )
+              }}
+            />
+
+            <Controller
+              control={control}
+              name="gameLocation"
+              render={({ field: { value, onChange } }) => (
+                <Select
+                  label="Game Location"
+                  value={value || ''}
+                  selectedKeys={[String(value)]}
+                  defaultSelectedKeys={[String(value)]}
+                  onChange={onChange}
+                >
+                  {['home', 'away', 'neutral', 'bye'].map((result) => (
+                    <SelectItem key={result} value={result}>
+                      {result}
+                    </SelectItem>
+                  ))}
+                </Select>
+              )}
+            />
+          </div>
+        )}
         <div className="flex flex-col gap-2 ">
           <h2 className="text-lg font-semibold">Score Summary</h2>
           <div className="grid grid-cols-6 grid-rows-3 text-center border-gray-300 bg-default-100 p-4 rounded">
@@ -162,14 +260,23 @@ const EditGame: React.FC<EditGameProps> = ({
               FINAL
             </span>
             <span className="flex items-center justify-center border-b">
-              {game.homeTeam?.school}
+              {game.awayTeamId !== game.homeTeamId
+                ? game.homeTeam?.school
+                : teamOptions?.find(
+                    (team) => team.value === String(watchedHomeTeamId)
+                  )?.label}
             </span>
             <div className="flex items-center justify-center border-b">
               <Controller
                 name="scoreSummary.1.home"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
@@ -178,7 +285,12 @@ const EditGame: React.FC<EditGameProps> = ({
                 name="scoreSummary.2.home"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
@@ -187,7 +299,12 @@ const EditGame: React.FC<EditGameProps> = ({
                 name="scoreSummary.3.home"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
@@ -196,7 +313,12 @@ const EditGame: React.FC<EditGameProps> = ({
                 name="scoreSummary.4.home"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
@@ -205,19 +327,33 @@ const EditGame: React.FC<EditGameProps> = ({
                 name="scoreSummary.final.home"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
             <span className="flex items-center justify-center border-b">
-              {game.awayTeam?.school}
+              {game.awayTeamId !== game.homeTeamId
+                ? game.awayTeam?.school
+                : teamOptions?.find(
+                    (team) => team.value === String(watchedAwayTeamId)
+                  )?.label}
             </span>
             <div className="flex items-center justify-center border-b">
               <Controller
                 name="scoreSummary.1.away"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
@@ -226,7 +362,12 @@ const EditGame: React.FC<EditGameProps> = ({
                 name="scoreSummary.2.away"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
@@ -235,7 +376,12 @@ const EditGame: React.FC<EditGameProps> = ({
                 name="scoreSummary.3.away"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
@@ -244,7 +390,12 @@ const EditGame: React.FC<EditGameProps> = ({
                 name="scoreSummary.4.away"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>
@@ -253,7 +404,12 @@ const EditGame: React.FC<EditGameProps> = ({
                 name="scoreSummary.final.away"
                 control={control}
                 render={({ field }) => (
-                  <Input size="lg" {...field} type="number" />
+                  <Input
+                    size="lg"
+                    {...field}
+                    value={field.value ? String(field.value) : undefined}
+                    type="number"
+                  />
                 )}
               />
             </div>

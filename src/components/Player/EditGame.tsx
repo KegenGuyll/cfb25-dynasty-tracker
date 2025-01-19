8d1026e3e91b36960cefc1f5648634d419a8c061
Kegen Guyll <guyllkegen@gmail.com>
2025-01-19 11:56:20 -0600

    Ensure teamOptions defaults to an empty array and update getTeamSelectOptions to return an empty array if no teams are found

--- src/components/Player/EditGame.tsx
+++ src/components/Player/EditGame.tsx
@@ -192,7 +192,7 @@ const EditGame: React.FC<EditGameProps> = ({
                   <label>
                     <span>Away Team</span>
                     <TeamSelect
-                      teamOptions={teamOptions}
+                      teamOptions={teamOptions || []}
                       value={String(value)}
                       onChange={onChange}
                     />
@@ -208,7 +208,7 @@ const EditGame: React.FC<EditGameProps> = ({
                   <label>
                     <span>Home Team</span>
                     <TeamSelect
-                      teamOptions={teamOptions}
+                      teamOptions={teamOptions || []}
                       value={String(value)}
                       onChange={onChange}
                     />
--- src/db/functions/getTeamSelectOptions.ts
+++ src/db/functions/getTeamSelectOptions.ts
@@ -1,7 +1,10 @@
+import { OptionType } from "@/components/SearchableSelect";
 import { db } from "../db.model";
 
-const getTeamSelectOptions = async () => {
+const getTeamSelectOptions = async (): Promise<OptionType[]> => {
   const teams = await db.teams.toArray();
+
+  if (!teams) return []
 
   return teams.map((team) => ({
     value: String(team.teamId),

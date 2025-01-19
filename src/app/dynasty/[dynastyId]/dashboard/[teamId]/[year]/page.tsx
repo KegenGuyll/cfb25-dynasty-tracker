5ee30ef362899df117b03fad7bc623b379db1de6
Kegen Guyll <guyllkegen@gmail.com>
2025-01-19 11:51:38 -0600

    Refactor Team Year Page layout to improve structure and add section divider

--- src/app/dynasty/[dynastyId]/dashboard/[teamId]/[year]/page.tsx
+++ src/app/dynasty/[dynastyId]/dashboard/[teamId]/[year]/page.tsx
@@ -3,6 +3,7 @@ import GameSummary from '@/components/DashboardSections/GameSummary'
 import RecruitingSection from '@/components/DashboardSections/Recruiting'
 import TeamOverview from '@/components/DashboardSections/TeamOverview'
 import TeamSchedule from '@/components/DashboardSections/TeamSchedule'
+import { Divider } from '@nextui-org/react'
 
 type TeamYearPageProps = {
   params: {
@@ -18,12 +19,30 @@ const TeamYearPage: React.FC<TeamYearPageProps> = ({
   const { dynastyId, teamId, year } = params
 
   return (
-    <div className="flex flex-col gap-12 w-full">
-      <TeamOverview dynastyId={dynastyId} teamId={teamId} year={year} />
-      <RecruitingSection dynastyId={dynastyId} teamId={teamId} year={year} />
-      <DraftResults />
-      <TeamSchedule teamId={+teamId} year={+year} dynastyId={dynastyId} />
-      <GameSummary teamId={+teamId} year={+year} />
+    <div className="w-full">
+      <div className="grid grid-flow-col grid-cols-7 gap-4 w-full">
+        <div className="border p-4 hidden lg:block lg:col-span-1"></div>
+        <div className="flex flex-col gap-12 w-full col-span-5 lg:col-span-4">
+          <h1 className="text-3xl font-bold">Team Year Page</h1>
+          <div className="flex flex-col gap-8">
+            <div>
+              <h2 className="text-2xl font-bold">Season</h2>
+              <Divider />
+            </div>
+            <RecruitingSection
+              dynastyId={dynastyId}
+              teamId={teamId}
+              year={year}
+            />
+          </div>
+          <DraftResults />
+          <TeamSchedule teamId={+teamId} year={+year} dynastyId={dynastyId} />
+          <GameSummary teamId={+teamId} year={+year} />
+        </div>
+        <div className="md:col-span-2">
+          <TeamOverview dynastyId={dynastyId} teamId={teamId} year={year} />
+        </div>
+      </div>
     </div>
   )
 }

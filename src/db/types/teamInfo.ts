c1e63fdb9d652e699ceb47a9a53752416f9068a9
Kegen Guyll <guyllkegen@gmail.com>
2025-01-19 11:54:57 -0600

    Add optional coaching and ranking fields to TeamInfo interface

--- src/db/types/teamInfo.ts
+++ src/db/types/teamInfo.ts
@@ -15,6 +15,14 @@ interface TeamInfo {
   teamLosses: number
   conferenceWins: number
   conferenceLosses: number
+  headCoach?: string
+  offensiveCoordinator?: string
+  defensiveCoordinator?: string
+  offPlaybook?: string
+  defPlaybook?: string
+  coachesPollRanking?: number
+  apPollRanking?: number
+  programPrestige?: number
 }
 
 export type {
